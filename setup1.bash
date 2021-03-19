f3045f6302899607c197478ae415aebe2a7ddc8d
Quang Truong <geniuszeroone01@gmail.com>
2021-03-19 11:26:58 +0000

    Remove unused files
    setup[1-2].bash were created for explanation in the book but they
    are not required for the system

--- setup1.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/RaspberryPiMouse-1/src/drivers/
-insmod rtmouse.ko
--- setup2.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xve
-
-exec 2> /tmp/setup.log
-
-cd /home/ubuntu/RaspberryPiMouse-1/src/drivers/
-/sbin/insmod rtmouse.ko
